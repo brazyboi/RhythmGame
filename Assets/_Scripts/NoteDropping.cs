977092325a7733bf2de21b636231876c533cf853
busyboy5483 <zhonglucas@gmail.com>
2021-06-05 13:46:11 -0700

    added basic note prefab

---
+++ Assets/_Scripts/NoteDropping.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteDropping : MonoBehaviour
+{
+    public float speed;
+    Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+
+        Vector3 screenPos = cam.WorldToScreenPoint(gameObject.transform.position);
+        if (screenPos.y > Screen.height + 200)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Assets/_Scripts/NoteSpawning.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteSpawning : MonoBehaviour
+{
+
+    public GameObject noteObj;
+    public float waitTime = 1;
+    float xPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(noteSpawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void spawnNote()
+    {
+
+
+
+    }
+    
+    IEnumerator noteSpawn()
+    {
+        while (true)
+        {
+            xPos = UnityEngine.Random.Range(-Screen.width + 200, Screen.width - 200) / 100;
+            Instantiate(noteObj, new Vector3(xPos, (float)(4.75), 0), Quaternion.identity);
+            yield return new WaitForSeconds(waitTime);
+
+        }
+    }
+
+}
