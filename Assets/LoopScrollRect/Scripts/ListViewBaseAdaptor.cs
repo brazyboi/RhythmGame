1f9337c3bbd5aa6839ad1c5a552b5f61653165cf
Jun Zhong <powersoundlab@gmail.com>
2021-06-26 23:12:29 -0700

    Add LoopScrollRect for scroll view

---
+++ Assets/LoopScrollRect/Scripts/ListViewBaseAdaptor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ListViewBaseAdaptor : MonoBehaviour {
+	public delegate void RefreshItems();
+
+	public RefreshItems refreshItems;
+
+	public abstract int getTotalCount ();
+
+	public abstract string getCellPrefab (); 
+
+	public abstract void bindCellData (int index, Transform prefabCell);
+}
