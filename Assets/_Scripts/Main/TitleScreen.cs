a6f536c1cd97550802e79aaa072536a1f43b6a67
Lucas Zhong <zhonglucas@gmail.com>
2021-06-30 21:33:22 -0700

    Add clickable playbutton on titlescreen

---
+++ Assets/_Scripts/Main/TitleScreen.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    public GameObject playButton;
+    public GameObject rewardButton;
+    public GameObject shareButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton.GetComponent<Button>().onClick.AddListener(startGame);
+
+    }
+
+    void startGame()
+    {
+        SceneManager.LoadScene("SongList", LoadSceneMode.Single);
+    }
+
+    void rewards()
+    {
+        SceneManager.LoadScene("RewardScreen", LoadSceneMode.Single);
+    }
+
+    void share()
+    {
+        // show popup for share
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
