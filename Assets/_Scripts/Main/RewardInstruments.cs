1f097e985aac973899ad74d31a1fe1c3c093f846
Lucas Zhong <zhonglucas@gmail.com>
2021-07-01 22:51:41 -0700

    Add Button Function To Rewards

--- Assets/_Scripts/Main/RewardInstruments.cs
+++ Assets/_Scripts/Main/RewardInstruments.cs
@@ -5,23 +5,90 @@ using UnityEngine.UI;
 
 public class RewardInstruments : MonoBehaviour
 {
-    public GameObject instrumentSelectBox;
 
-    Texture2D[] itemImages;
+    /*Texture2D[] itemImages;
     Sprite[] itemSprites;
 
     int[] xPoses;
-    int[] yPoses;
+    int[] yPoses;*/
 
-    int index;
+    public GameObject instrument1;
+    public GameObject instrument2;
+    public GameObject instrument3;
+    public GameObject instrument4;
+    public GameObject instrument5;
+    public GameObject instrument6;
+    public GameObject instrument7;
+    public GameObject instrument8;
+    public GameObject instrument9;
+    public GameObject instrument10;
+    public GameObject instrument11;
+    public GameObject instrument12;
+
+    GameObject[] itemButtons;
+
+    int[] musicIntruments;
 
     // Start is called before the first frame update
     void Start()
     {
-        initReward();
+
+        setMusicInstruments();
+
+        setUpItemButtons();
+
+
     }
 
-    void initReward()
+    void setMusicInstruments()
+    {           
+        musicIntruments = new int[12];
+
+        musicIntruments[0] = MusicInstrument.PIANO_INSTRUMENT;
+        musicIntruments[1] = MusicInstrument.FLUTE_INSTRUMENT;
+        musicIntruments[2] = MusicInstrument.SAX_INSTRUMENT;
+        musicIntruments[3] = MusicInstrument.VIOLIN_INSTRUMENT;
+        musicIntruments[4] = MusicInstrument.TRUMPET_INSTRUMENT;
+        musicIntruments[5] = MusicInstrument.HARMONICA_INSTRUMENT;
+        musicIntruments[6] = MusicInstrument.GUITAR_INSTRUMENT;
+        musicIntruments[7] = MusicInstrument.ELECTRIC_GUITAR_INSTRMENT;
+        musicIntruments[8] = MusicInstrument.ACCORDION_INSTRUMENT;
+        musicIntruments[9] = MusicInstrument.DIZI_INSTRUMENT;
+        musicIntruments[10] = MusicInstrument.OCARINA_INSTRUMENT;
+        musicIntruments[11] = MusicInstrument.XIAO_INSTRUMENT;
+    }
+
+    void setUpItemButtons()
+    {
+        itemButtons = new GameObject[12];
+        itemButtons[0] = instrument1;
+        itemButtons[1] = instrument2;
+        itemButtons[2] = instrument3;
+        itemButtons[3] = instrument4;
+        itemButtons[4] = instrument5;
+        itemButtons[5] = instrument6;
+        itemButtons[6] = instrument7;
+        itemButtons[7] = instrument8;
+        itemButtons[8] = instrument9;
+        itemButtons[9] = instrument10;
+        itemButtons[10] = instrument11;
+        itemButtons[11] = instrument12;
+
+        for (int i = 0; i < itemButtons.Length; i++)
+        {
+            int index = i;
+            itemButtons[i].GetComponent<Button>().onClick.AddListener(delegate { onSelectInstrument(index); });
+        }
+    }
+
+    void onSelectInstrument(int index)
+    {
+
+        AppContext.instance().setInstrument(musicIntruments[index]);
+        Debug.Log("index: " + index + " Current instrument index: " + AppContext.instance().getInstrument());
+    }
+
+    /*void initReward()
     {
         itemImages = Resources.LoadAll("InstrumentImages") as Texture2D[];
         Debug.Log(itemImages.Length);
@@ -62,16 +129,11 @@ public class RewardInstruments : MonoBehaviour
             instrumentSelectBox.GetComponent<Image>().sprite = itemSprites[i];
             Instantiate(instrumentSelectBox, new Vector3(xPoses[i % xPoses.Length], 50f, 0), Quaternion.identity);
         }
-    }
+    }*/
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    void onSelectInstrument()
-    {
-
     }
 }
