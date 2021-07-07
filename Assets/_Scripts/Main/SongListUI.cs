dabf42bd99794322586fd8aa0fd4478fa3f255c8
Lucas Zhong <zhonglucas@gmail.com>
2021-07-06 21:31:38 -0700

    Minor improvements to SongList UI

---
+++ Assets/_Scripts/Main/SongListUI.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SongListUI : MonoBehaviour
+{
+    public GameObject backButton;
+    public GameObject songsTextTop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        init();
+    }
+
+    void init()
+    {
+        songsTextTop.GetComponent<Text>().text = "SONGS: LEVEL " + (AppContext.instance().curSongListLevel + 1);
+        backButton.GetComponent<Button>().onClick.AddListener(backToLevelScreen);
+    }
+
+    void backToLevelScreen()
+    {
+        GameManager.gotoLevelScreen();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
